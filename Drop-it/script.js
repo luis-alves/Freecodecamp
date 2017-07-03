09edcdfcefdf272e1b906bb78fe88dd310e490f9
Luis Alves <lmoalves@gmail.com>
2017-07-03 11:46:52 +0100

    First commit of Drop it exercise

---
+++ Drop-it/script.js
@@ -0,0 +1,11 @@
+/**
+  * FreeCodeCamp Algorithm exercise
+  * Drop it
+  */
+
+function dropElements(arr, func) {
+  // Drop them elements.
+  return arr;
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
