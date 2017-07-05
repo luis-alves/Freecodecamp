fc1775ff16ec9103acbb486d2786fdbc5b7eddcd
Luis Alves <lmoalves@gmail.com>
2017-07-05 18:06:52 +0100

    First commit of Validate US Telephone Numbers

---
+++ Advanced-Algorithm-Scripting/Validate-US-Telephone-Numbers/script.js
@@ -0,0 +1,13 @@
+/**
+  * FreeCodeCamp Algorithm exercise
+  * Validate US Telephone Numbers
+  */
+
+function telephoneCheck(str) {
+  // Good luck!
+  return true;
+}
+
+
+
+telephoneCheck("555-555-5555");
