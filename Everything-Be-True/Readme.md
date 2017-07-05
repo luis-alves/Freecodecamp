c3d6d08e2ad5ffa1396f54fd121be789243e8706
Luis Alves <lmoalves@gmail.com>
2017-07-05 17:07:33 +0100

    Final commit of Everything Be True

--- Everything-Be-True/Readme.md
+++ Everything-Be-True/Readme.md
@@ -6,57 +6,13 @@ Remember, you can access object properties through either dot notation or [] not
 
 ## First considerations
 
-
+Check if 2nd argument of given function when used as key in array of dictionaries
+has valid value.
 
 ## My way of doing it
 
-1. 
+1. Series of if statments checking for valid values.
 
 
 ## Methods used
-### Array.isArray()
-
-[See Mozilla Developer Network](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray)
-
-#### Syntax
-`Array.isArray(obj)`
-
-This method determines if the element is an Array object.
-
-#### Example
-```
-const str = ["Hello", 'mom'];
-
-const newStr = Array.isArray(str);
-console.log(newStr);
-
-```
-output: *True*
-
-### push()
-
-[See Mozilla Developer Network](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
-
-#### Syntax
-`arr.push([element1[, ...[, elementN]]])`
-
-This method is used in array, as a way to insert an new element at the end
-of the array. The opposing method is the `pop()`.
-
-#### Example
-```
-const arr = [];
-const a = 'one';
-const a = 'two';
-
-const newArr.push(a);
-console.log(newArr);
-
-newArr.push(b);
-console.log(newArr);
-
-
-```
-output 1: ['a']
-output 2: ['a', 'b']
-                 ^-------- the char is placed at the end of array
+No methods used.
--- Everything-Be-True/script.js
+++ Everything-Be-True/script.js
@@ -3,26 +3,61 @@
   * Everything Be True
   */
 
-  function truthCheck(collection, pre) {
-    
-
-  return pre;
+function truthCheck(collection, pre) {
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i][pre] === undefined || collection[i][pre] === null ||
+       collection[i][pre] === 0 || collection[i][pre] === '' ||
+       (isNaN(collection[i][pre]) && typeof collection[i][pre] !== 'string')) {
+      return false;
+    }
+  }
+  return true;
 }
-
-
-
-
-
-
 
 
 // Testing
 
 console.log('Tests:');
 console.log('');
-console.log('My answer is: ' + truthCheck([{"user": "Tinky-Winky", "sex": "male"}, \
-{"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, \
-{"user": "Po", "sex": "female"}], "sex"));
+console.log('My answer is: ' + truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
 console.log('The corrected answer is: "true"');
-truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, \
-{"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") === true ? console.log('Passed') : console.log('Failed');
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") === true ? console.log('Passed') : console.log('Failed');
+console.log('-------------------------------');
+console.log('My answer is: ' + truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+console.log('The corrected answer is: "false"');
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") === false ? console.log('Passed') : console.log('Failed');
+console.log('-------------------------------');
+console.log('My answer is: ' + truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age"));
+console.log('The corrected answer is: "false"');
+truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0},
+{"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5},
+{"user": "Po", "sex": "female", "age": 4}], "age") === false ? console.log('Passed') : console.log('Failed');
+console.log('-------------------------------');
+console.log('My answer is: ' + truthCheck([{"name": "Pete", "onBoat": true},
+{"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat"));
+console.log('The corrected answer is: "false"');
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true},
+{"name": "FastFoward", "onBoat": null}], "onBoat") === false ? console.log('Passed') : console.log('Failed');
+console.log('-------------------------------');
+console.log('My answer is: ' + truthCheck([{"name": "Pete", "onBoat": true},
+{"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat"));
+console.log('The corrected answer is: "true"');
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"},
+{"name": "FastFoward", "onBoat": true}], "onBoat") === true ? console.log('Passed') : console.log('Failed');
+console.log('-------------------------------');
+console.log('My answer is: ' + truthCheck([{"single": "yes"}], "single"));
+console.log('The corrected answer is: "true"');
+truthCheck([{"single": "yes"}], "single") === true ? console.log('Passed') : console.log('Failed');
+console.log('-------------------------------');
+console.log('My answer is: ' + truthCheck([{"single": ""}, {"single": "double"}], "single"));
+console.log('The corrected answer is: "false"');
+truthCheck([{"single": ""}, {"single": "double"}], "single") === false ? console.log('Passed') : console.log('Failed');
+console.log('-------------------------------');
+console.log('My answer is: ' + truthCheck([{"single": "double"}, {"single": undefined}], "single"));
+console.log('The corrected answer is: "false"');
+truthCheck([{"single": "double"}, {"single": undefined}], "single") === false ? console.log('Passed') : console.log('Failed');
+console.log('-------------------------------');
+console.log('My answer is: ' + truthCheck([{"single": "double"}, {"single": NaN}], "single"));
+console.log('The corrected answer is: "false"');
+truthCheck([{"single": "double"}, {"single": NaN}], "single") === false ? console.log('Passed') : console.log('Failed');
+console.log('-------------------------------');
